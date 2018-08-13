51c26a5a04115292b61881befb60310d01ca1094
Charles <2477889605@qq.com>
2018-08-13 16:46:00 +0800

    optimize code

--- src/main/java/com/charles/na/soa/impl/thread/BuildDocumentVectorThread.java
+++ src/main/java/com/charles/na/soa/impl/thread/BuildDocumentVectorThread.java
@@ -38,9 +38,7 @@ public class BuildDocumentVectorThread extends Thread {
 
     private OptRecordMapper optRecordMapper;
 
-    public BuildDocumentVectorThread() {
-        super();
-    }
+    private List<Integer> threads;
 
     public BuildDocumentVectorThread(Map<String, Object> pageInfo) {
         this.pageInfo = pageInfo;
@@ -48,55 +46,62 @@ public class BuildDocumentVectorThread extends Thread {
 
     @Override
     public void run() {
-        if (pageInfo == null || pageInfo.get("pageNo") == null
-                || pageInfo.get("pageSize") == null || pageInfo.get("date") == null) {
-            LOGGER.error("分页信息缺失");
-            return;
-        }
-        List<News> newsList = null;
         try {
-            newsList = queryDBToGetNews(pageInfo);
-            System.out.println(newsList.size());
-        } catch (Exception e2) {
-            e2.printStackTrace();
-            LOGGER.error("查询t_news出现问题");
-            return;
-        }
-        int totalNum = 0;
-        for (News news : newsList) {
-            try {
-                totalNum++; //记录
-                DocumentVector dv = new DocumentVector();
-                dv.setId(IDUtil.generateLongID());
-                dv.setNewsId(news.getId());
-                Map<String, Integer> map = newsService.splitById(news.getId());
-                String vector = "(";
-                for (Map.Entry<String, Integer> e : map.entrySet()) {
-                    vector = vector + e.getKey() + ":" + e.getValue() + " ";
-                }
-                vector = vector.substring(0, vector.length() - (vector.length() > 1 ? 1 : 0)) + ")";
-                dv.setVector(vector);
-                dv.setDate(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
-                //插入文本向量，同样使用失败重连机制
-                queryDBToInsertVector(dv);
-            } catch (Exception e1) {
-                totalNum--;
-                e1.printStackTrace();
-                LOGGER.error("在创建文本" + news.getId() + "时出现问题");
+            if (pageInfo == null || pageInfo.get("pageNo") == null
+                    || pageInfo.get("pageSize") == null || pageInfo.get("date") == null) {
+                LOGGER.error("分页信息缺失");
+                return;
             }
-        }
+            List<News> newsList = null;
+            try {
+                newsList = queryDBToGetNews(pageInfo);
+                System.out.println(newsList.size());
+            } catch (Exception e2) {
+                e2.printStackTrace();
+                LOGGER.error("查询t_news出现问题");
+                return;
+            }
+            int totalNum = 0;
+            for (News news : newsList) {
+                try {
+                    totalNum++; //记录
+                    DocumentVector dv = new DocumentVector();
+                    dv.setId(IDUtil.generateLongID());
+                    dv.setNewsId(news.getId());
+                    Map<String, Integer> map = newsService.splitById(news.getId());
+                    String vector = "(";
+                    for (Map.Entry<String, Integer> e : map.entrySet()) {
+                        vector = vector + e.getKey() + ":" + e.getValue() + " ";
+                    }
+                    vector = vector.substring(0, vector.length() - (vector.length() > 1 ? 1 : 0)) + ")";
+                    dv.setVector(vector);
+                    dv.setDate(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+                    //插入文本向量，同样使用失败重连机制
+                    queryDBToInsertVector(dv);
+                } catch (Exception e1) {
+                    totalNum--;
+                    e1.printStackTrace();
+                    LOGGER.error("在创建文本" + news.getId() + "时出现问题");
+                }
+            }
 
-        LOGGER.info("本次文档向量建立过程成功，分页信息:" + pageInfo);
-        //将操作成功的记录写入数据库
-        OptRecord optRecord = new OptRecord();
-        optRecord.setId(IDUtil.generateID());
-        optRecord.setOpt("建立文档向量模型成功，分页信息:" + pageInfo);
-        optRecord.setOptNum(totalNum);
-        optRecord.setTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-        optRecord.setStatus(1);
-        queryDBToInsertLog(optRecord);
-        //将统计用的线程执行数减一
-        NewsSOAServiceImpl.currentVectorThreadNum--;
+            LOGGER.info("本次文档向量建立过程成功，分页信息:" + pageInfo);
+            //将操作成功的记录写入数据库
+            OptRecord optRecord = new OptRecord();
+            optRecord.setId(IDUtil.generateID());
+            optRecord.setOpt("建立文档向量模型成功，分页信息:" + pageInfo);
+            optRecord.setOptNum(totalNum);
+            optRecord.setTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+            optRecord.setStatus(1);
+            queryDBToInsertLog(optRecord);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            synchronized (threads) {
+                threads.remove(0);
+            }
+            NewsSOAServiceImpl.waitOrNotify(threads, threads);
+        }
     }
 
     /**
@@ -193,5 +198,13 @@ public class BuildDocumentVectorThread extends Thread {
 
     public void setOptRecordMapper(OptRecordMapper optRecordMapper) {
         this.optRecordMapper = optRecordMapper;
+    }
+
+    public List<Integer> getThreads() {
+        return threads;
+    }
+
+    public void setThreads(List<Integer> threads) {
+        this.threads = threads;
     }
 }
